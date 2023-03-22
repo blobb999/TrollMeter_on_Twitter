f7eae629edb2b081b13318b96c858d1dfe46ae7b
blobb <89486466+blobb999@users.noreply.github.com>
2023-03-22 12:41:07 +0100

    constants.py
    to be extended individually

---
+++ constants.py
@@ -0,0 +1,11 @@
+SENTINEL_WORDS = {'schwurbler': 1, 'verschwörungstheoretiker': 1, 'vtler': 1, 'dunning-kruger': 1, 'flacherdler': 1, 'covidiot': 1, 'aluhut': 1, 'schwachsinn': 1, 'leerdenker': 1, 'querdenker': 1, 'paulanergarten': 1, 'nachweise': 1, 'beweise': 1, 'impfgegner': 1}
+
+AGGRESSIVENESS_WORDS = {'oberpfeife': 1, 'dummheit': 1, 'flasche': 1, 'vollpfosten': 1, 'idioten': 1, 'spinner': 1, 'rechtsextreme': 1, 'antisemit': 1, 'leugner': 1, 'rechter': 1}
+
+PRO_TOPICS = {'co2': 1, 'klimawandel': 1, 'transgender': 1, 'impfung': 1, 'flache erde': 1, 'flatearth': 1, 'alien': 1, 'wef': 1, 'bilderberger': 1, 'un': 1, 'who': 1}
+
+CONTRA_TOPICS = {'mondlandung': 1, 'chemtrails': 1, 'chemtrails-Anhänger': 1, 'impfskeptiker': 1, 'religion': 1, 'esoterik': 1, 'reichsbürger': 1}
+
+PRO_ACCOUNTS = {'@goldeneraluhut': 1, '@volksverpetzer': 1}
+
+CONTRA_ACCOUNTS = {'@danieleganser': 1, '@kenfm': 1}
